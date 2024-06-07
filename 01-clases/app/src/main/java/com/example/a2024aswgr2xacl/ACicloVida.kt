5b4bd84591dd2cf5d5905bdd7e8072daed7f0bfe
Xavier <xavier.carpio@epn.edu.ec>
2024-06-07 10:51:37 -0500

    "Clase 7 de junio"

--- 01-clases/app/src/main/java/com/example/a2024aswgr2xacl/ACicloVida.kt
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2xacl/ACicloVida.kt
@@ -5,16 +5,54 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class ACicloVida : AppCompatActivity() {
+
+    var textoGlobal=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_aciclo_vida)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_ciclo_vida)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+    fun mostrarSnackBar(texto:String){
+        textoGlobal+=texto
+        val snack= Snackbar.make(findViewById(R.id.cl_ciclo_vida),textoGlobal,
+            Snackbar.LENGTH_INDEFINITE)
+        snack.show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mostrarSnackBar("OnStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        mostrarSnackBar("OnResume")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        mostrarSnackBar("OnRestart")
+    }
+    override fun onPause() {
+        super.onPause()
+        mostrarSnackBar("OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mostrarSnackBar("OnStrop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mostrarSnackBar("OnDestroy")
+    }
+
+
 }
